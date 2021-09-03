7921cb838c508d95eb9c5d3b5d982fa237ff0c4a
Anjali299 <anjaliroy29999@gmail.com>
2021-09-03 11:09:56 +0530

    first

---
+++ app/src/main/java/com/example/redcanlogin/Help_com.java
@@ -0,0 +1,14 @@
+package com.example.redcanlogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Help_com extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help_com);
+    }
+}
---
+++ app/src/main/java/com/example/redcanlogin/lang_Com.java
@@ -0,0 +1,14 @@
+package com.example.redcanlogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class lang_Com extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lang__com);
+    }
+}
